32ced1e70f8b33b74018cd6d3d004133919f0863
sayar <sayars244@gmail.com>
2022-10-07 13:40:15 +0300

    Add project files.

---
+++ Dictionary/MyDictionary.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    public class MyDictionary<T, K>
+    {
+        KeyValuePair<T, K>[] items; // Veri Tipimizi Oluşturduk
+        
+        public MyDictionary()
+        {
+            items = new KeyValuePair<T, K>[0];
+        }
+        public void Add(T _key,K _value)
+        {
+            if(Control(_key))
+            {
+                Configuration();
+                items[items.Length - 1] = new KeyValuePair<T, K>(_key, _value); // Bize Gelen Değeri Diziye Aktardık
+
+            }
+            else
+            {
+                Console.WriteLine("Girdiniğiz Key Değeri Özel Olmalı. Daha Önce Eklemediğinizden Emin Olun.");
+            }
+        }
+
+        public void ShowList()
+        {
+            foreach (var item in items)
+            {
+                Console.WriteLine(item.Key + " " + item.Value);
+            }
+        }
+
+        private void Configuration()
+        {
+            KeyValuePair<T, K>[] tempArray = items; // Geçici Dizi oluşturma
+            items = new KeyValuePair<T, K>[items.Length + 1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i]; // Geçici Dizimizde ki Elemanlı Asıl Dizimize Koyduk.
+
+            }
+
+        }
+        private bool Control (T _key)
+        {
+            bool control = true;
+            for (int i = 0; i < items.Length; i++)
+            {
+                if (items[i].Key.ToString() == _key.ToString())
+                {
+                    control = false;
+                }
+            }
+            return control;
+        }
+    }
+}
---
+++ Dictionary/Program.cs
@@ -0,0 +1,8 @@
+﻿
+using Dictionary;
+
+MyDictionary<string, int> AdYas = new MyDictionary<string, int>();
+AdYas.Add("Semih", 19);
+AdYas.Add("Semih", 19);
+
+AdYas.ShowList();
